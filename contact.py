bd361bf34fcf6ec11b82f2770ae55a8aab5cb475
Kirill Sukhomlin <raznoobraznoe@gmail.com>
2021-02-08 23:34:03 +0300

    removed duplicate class

--- contact.py
+++ contact.py
@@ -46,10 +46,3 @@ class ContactSecondaryInfo:
         self.phone2 = phone2
         self.notes = notes
 
-
-class ContactSecondaryInfo:
-
-    def __init__(self, address2, phone2, notes):
-        self.address2 = address2
-        self.phone2 = phone2
-        self.notes = notes
