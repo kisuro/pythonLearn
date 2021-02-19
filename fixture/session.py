6928d0b84a8fdbe6bdb0e3cc9b200d9092ac0297
Kirill Sukhomlin <raznoobraznoe@gmail.com>
2021-02-19 12:04:05 +0300

    login / logout improvements

--- fixture/session.py
+++ fixture/session.py
@@ -16,3 +16,27 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        if wd.current_url == 'about:blank':
+            return False
+
+        return len(wd.find_elements_by_name("logout")) > 0
+
+    def ensure_login(self, username, pwd):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, pwd)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
