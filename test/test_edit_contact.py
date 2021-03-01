65958ffda1caff02874fe16f102e1a94fed22f06
Kirill Sukhomlin <raznoobraznoe@gmail.com>
2021-03-01 10:03:07 +0300

    check contacts lists(edit contact)

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,28 +3,34 @@ from model.contact import ContactInfo
 
 def test_edit_first_contact(app):
     if app.contact.amount() == 0:
-        app.contact.create(ContactInfo(firstname="KirillPrecondition", middlename="", lastname="Sukhomlin", nickname="kisuro",
-                                       title="mr", company="Deutsche Telekom", address="Piter", home="7000111",
-                                       mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
-                                       email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
-                                       bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
-                                       address2="AddressSecondary", phone2="7555555", notes="DrinkMe"))
-    app.contact.edit_first_contact(
-        ContactInfo(firstname="KirillEdit", middlename="Edit", lastname="SukhomlinEdit", nickname="kisuroEdit",
-                    title="mrEdit", company="Deutsche Telekom", address="Piter", home="7000111",
-                    mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
-                    email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
-                    bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
-                    address2="AddressSecondary", phone2="7555555", notes="DrinkMeEdit"))
+        app.contact.create(
+            ContactInfo(firstname="KirillPrecondition", middlename="", lastname="Sukhomlin", nickname="kisuro",
+                        title="mr", company="Deutsche Telekom", address="Piter", home="7000111",
+                        mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
+                        email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
+                        bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
+                        address2="AddressSecondary", phone2="7555555", notes="DrinkMe"))
+    contacts_before = app.contact.get_contact_list()
+    contact = ContactInfo(firstname="KirillEdit", middlename="Edit", lastname="SukhomlinEdit", nickname="kisuroEdit",
+                          title="mrEdit", company="Deutsche Telekom", address="Piter", home="7000111",
+                          mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
+                          email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
+                          bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
+                          address2="AddressSecondary", phone2="7555555", notes="DrinkMeEdit")
+    contact.id = contacts_before[0].id
+    app.contact.edit_first_contact(contact)
+    contacts_after = app.contact.get_contact_list()
+    contacts_before[0] = contact
+    assert sorted(contacts_before, key=ContactInfo.id_or_max) == sorted(contacts_after, key=ContactInfo.id_or_max)
 
-
-def test_edit_first_contact_middlename(app):
-    if app.contact.amount() == 0:
-        app.contact.create(ContactInfo(firstname="KirillPrecondition", middlename="", lastname="Sukhomlin", nickname="kisuro",
-                                       title="mr", company="Deutsche Telekom", address="Piter", home="7000111",
-                                       mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
-                                       email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
-                                       bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
-                                       address2="AddressSecondary", phone2="7555555", notes="DrinkMe"))
-    app.contact.edit_first_contact(
-        ContactInfo(middlename="UpdateMiddleOnly"))
+# def test_edit_first_contact_middlename(app):
+#    if app.contact.amount() == 0:
+#        app.contact.create(
+#            ContactInfo(firstname="KirillPrecondition", middlename="", lastname="Sukhomlin", nickname="kisuro",
+#                        title="mr", company="Deutsche Telekom", address="Piter", home="7000111",
+#                        mobile="7000112", work="7000113", fax="7000114", email="em1@t.com",
+#                        email2="em2@t.com", email3="em3@t.com", homepage="www.spb.com", bday="26",
+#                        bmonth="June", byear="1982", aday="3", amonth="April", ayear="1990",
+#                        address2="AddressSecondary", phone2="7555555", notes="DrinkMe"))
+#    app.contact.edit_first_contact(
+#        ContactInfo(middlename="UpdateMiddleOnly"))
