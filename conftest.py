224d163c4be352c2b36db0cb8ea0e6d5d00c2d75
Kirill Sukhomlin <raznoobraznoe@gmail.com>
2021-03-12 22:31:08 +0300

    add json properties file and methods for work with this file

--- conftest.py
+++ conftest.py
@@ -1,24 +1,34 @@
+import json
+import os.path
 import pytest
 from fixture.application import Application
 
+# define default value for variables
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
+    # define variables as global
     global fixture
+    global target
+    # get browser data as option (defined in run configuration "additional parameters", eg:--browser=chrome.
+    # default=firefox)
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    apwd = request.config.getoption("--apwd")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    if apwd == "undefined":
-        raise ValueError("Undefined admin pwd %s", apwd)
-    else:
-        fixture.session.ensure_login(username="admin", pwd=apwd)
+    # check if data from target.json not loaded - load it
+    if target is None:
+        # find path to file and join with filename (from option "target")
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        # open file only while load, then autoClose
+        with open(config_file) as f:
+            target = json.load(f)
+    # check if fixture not loaded - load it
+    if fixture is None or not fixture.is_valid():
+        # load base url from target.json
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    # load user/password from target.json
+    fixture.session.ensure_login(username=target['user'], pwd=target['password'])
     return fixture
 
 
@@ -27,12 +37,10 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
-    parser.addoption("--apwd", action="store", default="undefined")
+    parser.addoption("--target", action="store", default="target.json")
